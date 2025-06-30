a5258770aa74df924ace19e7743140eae0f23edd
Hakan7469 <hakancevi@yahoo.com>
2025-06-30 19:36:03 +0300

    1. parti

---
+++ components/RecommendationCard.tsx
@@ -0,0 +1,8 @@
+export default function RecommendationCard() {
+  return (
+    <div className="border rounded-lg p-4 mb-4">
+      <p className="line-clamp-4">Bu çok güzel bir ürün. Şiddetle tavsiye ediyorum.</p>
+      <button className="text-blue-500 mt-2">Devamını oku</button>
+    </div>
+  );
+}
