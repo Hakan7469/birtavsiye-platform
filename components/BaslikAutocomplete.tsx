6f7decd87890786fea9735260009b6c466076a18
Hakan7469 <hakancevi@yahoo.com>
2025-07-02 20:11:21 +0300

    autocomp

--- components/BaslikAutocomplete.tsx
+++ components/BaslikAutocomplete.tsx
@@ -38,4 +38,37 @@ export default function BaslikAutocomplete({ onSelect }: Props) {
 
   const handleSelect = (title: string) => {
     setInput(title);
-    setSugges
+    setSuggestions([]);
+    setShowSuggestions(false);
+    onSelect(title); // üst komponenti bilgilendir
+  };
+
+  return (
+    <div className="relative max-w-xl mx-auto mb-4">
+      <input
+        type="text"
+        placeholder="Başlık yazın..."
+        value={input}
+        onChange={(e) => {
+          setInput(e.target.value);
+          setShowSuggestions(true);
+        }}
+        className="w-full p-2 border rounded"
+      />
+
+      {showSuggestions && suggestions.length > 0 && (
+        <ul className="absolute z-10 bg-white border w-full mt-1 rounded shadow">
+          {suggestions.map((title, idx) => (
+            <li
+              key={idx}
+              className="px-4 py-2 hover:bg-blue-100 cursor-pointer"
+              onClick={() => handleSelect(title)}
+            >
+              {title}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
