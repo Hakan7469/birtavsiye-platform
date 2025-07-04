3598b40cf838cf4f9e58001faddc0da9a983f880
Hakan7469 <hakancevi@yahoo.com>
2025-07-04 18:12:54 +0300

    Update types.ts

--- lib/types.ts
+++ lib/types.ts
@@ -1,11 +1,16 @@
-export interface Tavsiye {
+import { Json } from './supabase'; // Eğer supabase tipi buradaysa, doğru import edilir
+
+export type Tavsiye = {
   id: string;
-  title: string;
-  content: string;
-  author: string;
-  created_at: string;
-  highlighted_text: any;
-  is_flagged?: boolean;
-  is_reviewed?: boolean;
-  review_notes?: string;
-}
+  uuid: string | null;
+  title: string | null;
+  content: string | null;
+  author: string | null;
+  created_at: string | null;
+  highlighted_text: Json;
+  is_flagged: boolean | null;
+  is_reviewed: boolean | null;
+  review_notes: string | null;
+  like: number;
+  dislike: number;
+};
