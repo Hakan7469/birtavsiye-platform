003569fad7a2a6c698e89502e156e56e54653e8f
Hakan7469 <hakancevi@yahoo.com>
2025-07-04 15:28:43 +0300

    Update supabase.ts

--- types/supabase.ts
+++ types/supabase.ts
@@ -1,56 +1,49 @@
-export type Json =
-  | string
-  | number
-  | boolean
-  | null
-  | { [key: string]: Json | undefined }
-  | Json[]
+export type Json = string | number | boolean | null | { [key: string]: Json } | Json[];
 
 export interface Database {
   public: {
     Tables: {
       recommendations: {
         Row: {
-          id: string
-          uuid: string | null
-          title: string | null
-          content: string | null
-          author: string | null
-          created_at: string | null
-          highlighted_text: Json | null
-          is_flagged: boolean | null
-          is_reviewed: boolean | null
-          review_notes: string | null
-        }
+          id: string;
+          uuid: string | null;
+          title: string | null;
+          content: string | null;
+          author: string | null;
+          created_at: string | null;
+          highlighted_text: Json;
+          is_flagged: boolean | null;
+          is_reviewed: boolean | null;
+          review_notes: string | null;
+        };
         Insert: {
-          id?: string
-          uuid?: string | null
-          title?: string | null
-          content?: string | null
-          author?: string | null
-          created_at?: string | null
-          highlighted_text?: Json | null
-          is_flagged?: boolean | null
-          is_reviewed?: boolean | null
-          review_notes?: string | null
-        }
+          id?: string;
+          uuid?: string | null;
+          title?: string | null;
+          content?: string | null;
+          author?: string | null;
+          created_at?: string | null;
+          highlighted_text?: Json;
+          is_flagged?: boolean | null;
+          is_reviewed?: boolean | null;
+          review_notes?: string | null;
+        };
         Update: {
-          id?: string
-          uuid?: string | null
-          title?: string | null
-          content?: string | null
-          author?: string | null
-          created_at?: string | null
-          highlighted_text?: Json | null
-          is_flagged?: boolean | null
-          is_reviewed?: boolean | null
-          review_notes?: string | null
-        }
-      }
-    }
-    Views: {}
-    Functions: {}
-    Enums: {}
-    CompositeTypes: {}
-  }
+          id?: string;
+          uuid?: string | null;
+          title?: string | null;
+          content?: string | null;
+          author?: string | null;
+          created_at?: string | null;
+          highlighted_text?: Json;
+          is_flagged?: boolean | null;
+          is_reviewed?: boolean | null;
+          review_notes?: string | null;
+        };
+      };
+    };
+    Views: {};
+    Functions: {};
+    Enums: {};
+  };
 }
