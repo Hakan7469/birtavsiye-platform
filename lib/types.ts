a8a5aa78de1b5b8e82280268e577ab6207efe474
Hakan7469 <hakancevi@yahoo.com>
2025-07-04 17:23:39 +0300

    Update types.ts

--- lib/types.ts
+++ lib/types.ts
@@ -1,14 +1,14 @@
 export interface Tavsiye {
   id: string;
-  uuid?: string | null;
-  title?: string | null;
-  content?: string | null;
-  author?: string | null;
-  created_at?: string | null;
-  highlighted_text?: any;
-  is_flagged?: boolean | null;
-  is_reviewed?: boolean | null;
-  review_notes?: string | null;
+  uuid: string | null;
+  title: string | null;
+  content: string | null;
+  author: string | null;
+  created_at: string | null;
+  highlighted_text: any;
+  is_flagged: boolean | null;
+  is_reviewed: boolean | null;
+  review_notes: string | null;
   like: number;
   dislike: number;
 }
