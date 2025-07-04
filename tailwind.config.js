7a9c0809d16c60edd088a704d39f970be5576f3f
Hakan7469 <hakancevi@yahoo.com>
2025-07-04 10:33:58 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  safelist: [
+    "text-sm",
+    "text-xs",
+    "text-gray-500",
+    "text-gray-600",
+    "border-gray-300",
+    "font-medium",
+    "hover:bg-blue-100",
+    "hover:bg-red-100",
+    "px-2",
+    "py-0.5",
+    "rounded"
   ],
   theme: {
     extend: {},
